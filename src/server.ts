e2ac7bc8596e118a569f09376a6b62126aadfc1a
riasat01 <riasat01@outlook.com>
2024-11-22 16:15:45 +0600

    chore: set up ESLint and Prettier - Installed and configured ESLint - Installed and configured Prettier - Added necessary dependencies and configuration files

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import app from "./app";
-import mongoose from "mongoose";
-import { dbUrl, port } from "./app/config";
-
+import app from './app';
+import mongoose from 'mongoose';
+import { dbUrl, port } from './app/config';
 
 const main = async () => {
-    try {
-        mongoose.connect(dbUrl as string);
-        app.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        })
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    mongoose.connect(dbUrl as string);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
