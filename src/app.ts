41c8d4274a441e633308498f368e851fc6e5ebc9
riasat01 <riasat01@outlook.com>
2024-11-24 19:37:21 +0600

    feat(error-handling): add global error handler and catch-all route - Implemented global error handler for consistent error responses - Added catch-all route to handle undefined routes

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,29 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
+app.get(`/`, (req: Request, res: Response) => {
+  res.status(200).json({
+    message: `Server is responding`,
+    success: true
+  });
+});
 app.use(`/api/products`, StationeryProductRouter);
 app.use(`/api/orders`, OrderRouter);
 
-app.get(`/`, (req: Request, res: Response) => {
-  res.send(`Server is saying hi there`);
+app.all(`*`, async (req: Request, res: Response) => {
+  res.status(500).json({
+    success: false,
+    message: `Route doesn't exist!`,
+  });
+});
+
+app.use((error: Error, req: Request, res: Response) => {
+  if (error) {
+    res.status(500).json({
+      success: false,
+      message: `Global error handler is saying something went wrong!`,
+    });
+  }
 });
 
 export default app;
